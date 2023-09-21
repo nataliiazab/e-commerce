1ca5c96a04ba0f5f0a6a10181ec6e04b4e33dd68
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-09-21 21:00:09 +0100

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,50 @@
-
+import React, { useState } from "react";
 import "./Header.css";
 
+
 function Header() {
+   const [isMenuOpen, setMenuOpen] = useState(false);
 
+   const toggleMenu = () => {
+     setMenuOpen(!isMenuOpen);
+   };
 
+   const closeMenu = () => {
+     setMenuOpen(false);
+   };
   return (
-    <div className="App">
-  to add header
-    </div>
+    <header>
+      <nav className="navbar">
+        <a href="#" className="nav-branding">
+          <ul className={`nav-menu ${isMenuOpen ? "active" : ""}`}>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                Products
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+
+          <div
+            className={`hamburger ${isMenuOpen ? "active" : ""}`}
+            onClick={toggleMenu}
+          >
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+        </a>
+      </nav>
+    </header>
   );
 }
 
